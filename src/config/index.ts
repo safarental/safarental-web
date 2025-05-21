07e5546ad1b83f378eb9ee0bc05be800be7b5d77
Safar Rental <connectsafarental@gmail.com>
2025-05-21 09:14:25 +0000

    ada kesalahan, anda tidak menulis /api nya, harusnya : https://larger-gourmet-peninsula-tribal.trycloudflare.com/api

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://larger-gourmet-peninsula-tribal.trycloudflare.com';
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://larger-gourmet-peninsula-tribal.trycloudflare.com/api';
 
