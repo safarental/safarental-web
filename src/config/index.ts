761825304060eb5985fab1f77d18b5ed73665989
Safar Rental <connectsafarental@gmail.com>
2025-05-21 08:26:34 +0000

    update

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://aef8-2001-448a-20a2-117e-7975-5c70-d28d-3470.ngrok-free.app/api';
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://larger-gourmet-peninsula-tribal.trycloudflare.com/api';
